f1e12e16f2a23225561ecff188b7726de4375dbd
Yaswanth <yeshwanthg@henote.net>
2018-06-29 08:45:11 +0530

    First Commit

--- ReorderTableViewCellsDemo/ViewController.swift
+++ ReorderTableViewCellsDemo/ViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var fruitsTbl : UITableView!
+    var fruitsArr = ["Mango", "Banana", "Apple", "Custard apple", "Pineapple", "Orange", "Grapes", "Kiwifruit", "Jackfruit", "Mudapples"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        fruitsTbl.tableFooterView = UIView()
+        fruitsTbl.isEditing = !fruitsTbl.isEditing
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +22,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    //MARK: TableView DataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruitsArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "")
+        cell.textLabel?.text = fruitsArr[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
+    }
+    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    //MARK: TableView Delegate Methods
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedObject = fruitsArr[sourceIndexPath.row]
+        fruitsArr.remove(at: sourceIndexPath.row)
+        fruitsArr.insert(movedObject, at: destinationIndexPath.row)
+    }
 
 }
 
